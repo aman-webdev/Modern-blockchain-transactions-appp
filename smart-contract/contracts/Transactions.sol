c447cfb815eac0e74c1d85f2c72972a679adb1b2
Aman <aman.nabi02@gmail.com>
2022-01-23 21:48:29 +0530

    Completed Frontend

---
+++ smart-contract/contracts/Transactions.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Transactions{
+    uint TransactionCounter;
+    event Transfer(address from,address receiver, uint amount,string message, uint256 timestamp, string keyword);
+
+    struct TransferStruct{
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    TransferStruct[] transactions;
+}
